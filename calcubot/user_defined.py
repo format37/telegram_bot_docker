51fc5b37ea59d2d89aa7bf5d5390cad1dbbe1499
alex <format37@gmail.com>
2022-06-30 21:34:00 +0300

    new file:   calcubot/user_defined.py

---
+++ calcubot/user_defined.py
@@ -0,0 +1,32 @@
+import math
+
+def nf(value,format_line='{:,}'):
+    return format_line.format(value)
+
+def fact(n):
+
+    factors = []
+
+    def get_factor(n):
+        x_fixed = 2
+        cycle_size = 2
+        x = 2
+        factor = 1
+
+        while factor == 1:
+            for count in range(cycle_size):
+                if factor > 1: break
+                x = (x * x + 1) % n
+                factor = math.gcd(x - x_fixed, n)
+
+            cycle_size *= 2
+            x_fixed = x
+
+        return factor
+
+    while n > 1:
+        next = get_factor(n)
+        factors.append(next)
+        n //= next
+
+    return factors
