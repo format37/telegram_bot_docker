57e6505791b4a19007af8760cbb9ec2194b9985b
alex <format37@gmail.com>
2022-06-30 23:09:54 +0300

    modified:   calcubot/calculate.py

--- calcubot/calculate.py
+++ calcubot/calculate.py
@@ -1,14 +1,14 @@
 import resource
 from sys import argv
 from user_defined import *
-import pandas as pd
-import numpy as np
-import scipy
-from scipy import stats
+#import pandas as pd
+#import numpy as np
+#import scipy
+#from scipy import stats
 import random
 import datetime as dt
-import statistics
-import ast
+#import statistics
+#import ast
 import math
 
 try:
