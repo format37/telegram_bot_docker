58c1867892f32bb3fa1574e6792ff6496fa0dbe1
alex <format37@gmail.com>
2022-07-01 16:16:21 +0300

    modified:   calcubot/server.py

--- calcubot/server.py
+++ calcubot/server.py
@@ -18,14 +18,14 @@ async def call_message(request):
     # load data
     data = json.loads(await request.json())
     expression = data['message']
+    # if expression contains '/cl ', remove it
+    if expression.startswith('/cl '):
+        expression = expression[4:]
     inline = int(data['inline'])
     answer_max_lenght       = 4095
     res = str(secure_eval(expression))[:answer_max_lenght]
     # prepare response
-    if inline == 0:
-        # if res contains '/cl ', remove it
-        if res.startswith('/cl '):
-            res = res[4:]
+    if inline == 0:        
         response = json.dumps(res + ' = ' + expression)
         return web.Response(text=response, content_type='application/json')
     else:
