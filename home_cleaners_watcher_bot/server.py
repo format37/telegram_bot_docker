5aa5aae901cdf49a1dd2717562700ffd295a4b7b
alex <format37@gmail.com>
2022-07-01 19:50:31 +0300

    modified:   home_cleaners_watcher_bot/server.py

--- home_cleaners_watcher_bot/server.py
+++ home_cleaners_watcher_bot/server.py
@@ -22,7 +22,7 @@ def cleaner_bot_counter_plus(account_id,task):
     df_full.loc[(df_full.account_id==account_id),task]=current_value+round(appendix,1)
     df_full.to_csv('data/data.csv',index = None)
     return task+' +'+str(round(appendix,1))
-    
+
 
 def cleaner_bot_counter_minus(account_id,task):	
 	df_full = pd.read_csv('data/data.csv')
@@ -65,13 +65,19 @@ async def call_message(request):
             answer = cleaner_bot_counter_plus(int(user), task)
 
         if task == 'cancel':
-            answer = cleaner_bot_counter_minus(int(user), task)
+            # extract thing to cancel from message
+            # for example, message:
+            # /cancel dish
+            # thing:
+            # dish
+            thing = message.split(' ')[1]
+            answer = cleaner_bot_counter_minus(int(user), thing)
     
     return web.Response(text=answer, content_type='application/json')
 
 
 def main():
-    app = web.Application(client_max_size=1024**3)    
+    app = web.Application(client_max_size=1024**3)
     app.router.add_route('POST', '/message', call_message)    
     web.run_app(app, port=os.environ.get('PORT', ''))
 
