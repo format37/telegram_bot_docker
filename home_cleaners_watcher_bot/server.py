d7faef44f704aefc7121f83c67fc1a723e6c88f0
alex <format37@gmail.com>
2022-07-01 19:47:38 +0300

    modified:   home_cleaners_watcher_bot/server.py

--- home_cleaners_watcher_bot/server.py
+++ home_cleaners_watcher_bot/server.py
@@ -6,26 +6,34 @@ import pandas as pd
 
 
 def cleaner_bot_counter_plus(account_id,task):
-    try:
-        account_id = int(account_id)
-        df_full = pd.read_csv('data/data.csv')
-        current_value = int(df_full[df_full.account_id==account_id][task])
-        df_task = df_full[['account_id', task]]
+    df_full = pd.read_csv('data/data.csv')
+    current_value = int(df_full[df_full.account_id==account_id][task])
+    df_task = df_full[['account_id', task]]
 
-        # Economics ++
-        account_value = float(df_task[df_task.account_id==account_id][task])
-        appendix = 1
-        if len(df_task)>1 and account_value>df_task[task].min():
-            for idx,row in df_task.iterrows():
-                if account_value>row[task]:
-                    appendix += (account_value-row[task])/10
-        # Economics --
+    # Economics ++
+    account_value = float(df_task[df_task.account_id==account_id][task])
+    appendix = 1
+    if len(df_task)>1 and account_value>df_task[task].min():
+        for idx,row in df_task.iterrows():
+            if account_value>row[task]:
+                appendix += (account_value-row[task])/10
+    # Economics --
 
-        df_full.loc[(df_full.account_id==account_id),task]=current_value+round(appendix,1)
-        df_full.to_csv('data/data.csv',index = None)
-        return task+' +'+str(round(appendix,1))
-    except Exception as e:
-        return 'Error: '+str(e)
+    df_full.loc[(df_full.account_id==account_id),task]=current_value+round(appendix,1)
+    df_full.to_csv('data/data.csv',index = None)
+    return task+' +'+str(round(appendix,1))
+    
+
+def cleaner_bot_counter_minus(account_id,task):	
+	df_full = pd.read_csv('data/data.csv')
+	if task in df_full.columns:
+		current_value = int(df_full[df_full.account_id==account_id][task])
+		df_full.loc[(df_full.account_id==account_id),task]=current_value-1
+		df_full.to_csv('data/data.csv',index = None)
+		answer = task+' -1'
+	else:
+		answer = 'task not found: "'+task+'"'
+	return answer
 
 
 def cleaner_bot_user_authorized(account_id):
@@ -44,20 +52,20 @@ async def call_message(request):
     if not cleaner_bot_user_authorized(user):
         return web.Response(text='User not authorized')
 
+    answer = 'Command not found'
+
     # extract command from message
     # for example, message:
     # /dish@home_cleaners_watcher_bot
     # command:
     # dish
     task = message.split('@')[0].split('/')[1]    
-    if task in ['dish', 'garbage', 'toilet', 'dry'] \
-        and group == cleaning_group_id:
-        answer = cleaner_bot_counter_plus(user, task)
-    else:
-        answer = 'Command not supported '+message
-        answer += '\nGroup: '+group
-        answer += '\nUser: '+user
-        answer += '\nTask: '+task
+    if group == cleaning_group_id:
+        if task in ['dish', 'garbage', 'toilet', 'dry']:
+            answer = cleaner_bot_counter_plus(int(user), task)
+
+        if task == 'cancel':
+            answer = cleaner_bot_counter_minus(int(user), task)
     
     return web.Response(text=answer, content_type='application/json')
 
