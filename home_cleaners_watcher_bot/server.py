98b0ac119f33de437f2201d3ff25b8de50051c98
alex <format37@gmail.com>
2022-07-01 19:31:28 +0300

    modified:   home_cleaners_watcher_bot/server.py

--- home_cleaners_watcher_bot/server.py
+++ home_cleaners_watcher_bot/server.py
@@ -51,7 +51,7 @@ async def call_message(request):
         answer = cleaner_bot_counter_plus(user, task)
     else:
         answer = 'Command not supported '+message
-        answer += ' '+group
+        answer += ' '+str(group)
         #answer += '\n'+user
         #answer += '\n'+task
     
