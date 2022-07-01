34638aa5f60b9288d5d128db316b33853588f131
alex <format37@gmail.com>
2022-07-01 19:41:50 +0300

    modified:   home_cleaners_watcher_bot/server.py

--- home_cleaners_watcher_bot/server.py
+++ home_cleaners_watcher_bot/server.py
@@ -6,22 +6,25 @@ import pandas as pd
 
 
 def cleaner_bot_counter_plus(account_id,task):
-	df_full = pd.read_csv('data/data.csv')
-	current_value = int(df_full[df_full.account_id==account_id][task])
-	df_task = df_full[['account_id', task]]
+    try:
+        df_full = pd.read_csv('data/data.csv')
+        current_value = int(df_full[df_full.account_id==account_id][task])
+        df_task = df_full[['account_id', task]]
 
-	# Economics ++
-	account_value = float(df_task[df_task.account_id==account_id][task])
-	appendix = 1
-	if len(df_task)>1 and account_value>df_task[task].min():
-		for idx,row in df_task.iterrows():
-			if account_value>row[task]:
-				appendix += (account_value-row[task])/10
-	# Economics --
+        # Economics ++
+        account_value = float(df_task[df_task.account_id==account_id][task])
+        appendix = 1
+        if len(df_task)>1 and account_value>df_task[task].min():
+            for idx,row in df_task.iterrows():
+                if account_value>row[task]:
+                    appendix += (account_value-row[task])/10
+        # Economics --
 
-	df_full.loc[(df_full.account_id==account_id),task]=current_value+round(appendix,1)
-	df_full.to_csv('data/data.csv',index = None)
-	return task+' +'+str(round(appendix,1))
+        df_full.loc[(df_full.account_id==account_id),task]=current_value+round(appendix,1)
+        df_full.to_csv('data/data.csv',index = None)
+        return task+' +'+str(round(appendix,1))
+    except Exception as e:
+        return 'Error: '+str(e)
 
 
 def cleaner_bot_user_authorized(account_id):
@@ -46,15 +49,14 @@ async def call_message(request):
     # command:
     # dish
     task = message.split('@')[0].split('/')[1]    
-    """
     if task in ['dish', 'garbage', 'toilet', 'dry'] \
         and group == cleaning_group_id:
         answer = cleaner_bot_counter_plus(user, task)
-    else:"""
-    answer = 'Command not supported '+message
-    answer += '\nGroup: '+group
-    answer += '\nUser: '+user
-    answer += '\nTask: '+task
+    else:
+        answer = 'Command not supported '+message
+        answer += '\nGroup: '+group
+        answer += '\nUser: '+user
+        answer += '\nTask: '+task
     
     return web.Response(text=answer, content_type='application/json')
 
