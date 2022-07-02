99d11909bbf5c4574dba78014281bc56edeb7248
alex <format37@gmail.com>
2022-07-02 14:56:26 +0300

    modified:   server/server.py

--- server/server.py
+++ server/server.py
@@ -88,14 +88,15 @@ def send_user(message):
     # application/json
     # image/png
     # Check the content type
-    if answer.headers['Content-Type'] == 'application/json':
+    if answer.headers['Content-Type'] == 'image/png':
+        hcwbot.send_photo(message.chat.id, answer.content, reply_to_message_id = str(message))
+    else: 
+        #  answer.headers['Content-Type'] == 'application/json':
         #if message.chat.id == message.from_user.id:
         #    cleaning_group_id = -37549110
         #    hcwbot.send_message(cleaning_group_id, answer.text)
         #else:
-        hcwbot.reply_to(message, answer.text)
-    elif answer.headers['Content-Type'] == 'image/png':
-        hcwbot.send_photo(message.chat.id, answer.content, reply_to_message_id = str(message))
+        hcwbot.reply_to(message, answer.text)    
 
 # === home_cleaners_watcher_bot --
 
