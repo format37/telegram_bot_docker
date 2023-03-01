c9c15cbb7713b781ef2fdba885132ba3213be892
alex <format37@gmail.com>
2023-03-02 00:39:08 +0400

    modified:   executebot/server.py

--- executebot/server.py
+++ executebot/server.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, jsonify, Response
 import logging
+import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,19 +11,24 @@ app = Flask(__name__)
 
 @app.route("/test")
 def call_test():
-  return "get ok"
+    return "get ok"
 
 @app.route("/request", methods=["POST"])
 def call_request():
-	r = request.get_json()
-	logger.info("request data: {}".format(r))
-	user_id = r["user_id"]
-	return jsonify({"text": user_id})
+    r = request.get_json()
+    logger.info("request data: {}".format(r))
+    # INFO:__main__:request data: {"user_id": 106129214}
+    # Assuming r is a JSON-formatted string
+    r_dict = json.loads(r)
+    user_id = r_dict["user_id"]
+    logger.info("user_id: {}".format(user_id))
+    # INFO:__main__:user_id: 106129214
+    return jsonify({"user_id": user_id})
 
 
 if __name__ == "__main__":
     app.run(
-		host='0.0.0.0',
-		debug=False,
-		port=int(os.environ.get("PORT", 10000))
-		)
+        host='0.0.0.0',
+        debug=False,
+        port=int(os.environ.get("PORT", 10000))
+        )
