39f1c3a85cd3c35b2ead7a365cf928306bc7a0f3
alex <format37@gmail.com>
2023-04-09 10:02:14 +0400

    new file:   logs_calcubot.sh

---
+++ logs_calcubot.sh
@@ -0,0 +1,7 @@
+# get id of running container with image name
+container_id=$(sudo docker ps | grep telegram_bot_docker_calcubot | awk '{print $1}')
+echo "container id: $container_id"
+# get logs
+sudo docker logs -f $container_id
+# connect to container
+# sudo docker exec -it $container_id /bin/bash
