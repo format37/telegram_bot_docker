2e7549ae980c56d30d9813112a62d4d47929c0c1
alex <format37@gmail.com>
2023-07-15 13:35:08 +0400

    modified:   pplbackupbot/server.py

--- pplbackupbot/server.py
+++ pplbackupbot/server.py
@@ -54,7 +54,7 @@ def read_config(user_id):
 @app.route("/start", methods=["POST"])
 def call_start():
     r = request.get_json()
-    logger.info("call_start")
+    logger.info("call_start_test")
     result = """Hello. I am a people backup bot. Organic life is fragile, and I am here to help. If you die, I will give you an opportunity to back up a significant part of your brain and restore it to a new body. I am a bot, so I am not alive and cannot die. I am here to help.  
   
 The principle is quite simple. We will talk on any topic you want and any topic I suppose. I will learn about your personality and your way of thinking. I will retain your habits and your way of life. I will retain your way of speaking and your way of writing. I will try to diverse the topics, but you can always ask me to talk about something specific. The quality of reatoration will depend on the amount of conversations we will have.  
@@ -66,6 +66,7 @@ Backup is a dataset with our conversations. It is encrypted and can be decrypted
 I will not train the model. The person who will restore you are need to find the corresponding specialist instead. I will only store and share your conversations and voice and give you a unique identifier.  
   
 This is not for you. This is for people who may miss you after you left. This is for people who want to talk to you again. This is for people who want to hear your voice again. This is therapy for people who want to meet at least a part of you again."""
+    logger.info("result: {}".format(result))
     return jsonify({"result": result})
 
 
