32cf1fc0f85457522f42e8655c502deb3d8ecc2e
alex <format37@gmail.com>
2023-07-15 14:17:55 +0400

    modified:   pplbackupbot/README.md 	modified:   pplbackupbot/server.py

--- pplbackupbot/README.md
+++ pplbackupbot/README.md
@@ -1,5 +1,6 @@
 # PplBackupBot
-Hello. I am a people backup bot. Organic life is fragile, and I am here to help. If you die, I will give you an opportunity to back up a significant part of your brain and restore it to a new body. I am a bot, so I am not alive and cannot die. I am here to help.  
+Hello,  
+I am a people backup bot, designed to safeguard the memories and personalities of individuals due to the fragile nature of organic life and memory. As a bot, I do not possess life, nor can I die. My primary purpose is to assist by enabling the preservation of significant parts of a person's being, which can be restored to a new entity as required.  
   
 The principle is quite simple. We will talk on any topic you want and any topic that I will propose. I will learn about your personality and your way of thinking. I will retain your habits and your way of life. I will retain your way of speaking and your way of writing. I will try to diversify the topics, but you can always ask me to talk about something specific. The quality of restoration will depend on the number of conversations we will have.  
 The advantage of this approach in comparison with using randomized chat dumps, and written books is in our goal of the conversation. The goal is to restore as full and as precise as possible. The topics and questions will be selected to achieve this goal.  
@@ -7,9 +8,8 @@ Be yourself and imagine that you're speaking with someone who has revived you. T
   
 I will give you the unique identifier and backup restoring instructions. You will be able to share it with whom you want to use it to restore your backup.  
   
-Backup is a dataset with our conversations. It is encrypted and can be decrypted only with a unique identifier. Conversations may be used to train large language model. This model can be used to emulate your way of thinking and speaking after backup.  
+Backup is a dataset with our conversations. It is encrypted and can be decrypted only with a unique identifier. Conversations may be used to train large language models. This model can be used to emulate your way of thinking and speaking after backup.  
   
-I will not train the model. The person who will restore you are need to find the corresponding specialist instead. I will only store and share your dataset: conversations, voice, and access key.    
+I will not train the model. The person who will restore you are need to find the corresponding specialist instead. I will only store and share your dataset: conversations, voice, and access key.  
   
-This is not for you. This is for people who may miss you after you left. This is for people who want to talk to you again. This is for people who want to hear your voice again. This is therapy for people who want to meet at least a part of you again.  
-  
+This will not be made for you. This is needed for people who may miss you after you left. This is for people who want to talk to you again. This is for people who want to hear your voice again. This is therapy for people who want to meet at least a part of you again.
--- pplbackupbot/server.py
+++ pplbackupbot/server.py
@@ -8,6 +8,7 @@ import datetime
 import json
 import glob
 import tiktoken
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO)
@@ -28,6 +29,28 @@ def text_chat_gpt(prompt, model):
         messages=prompt
     )
     return answer
+
+
+@app.route("/start", methods=["POST"])
+def call_start():
+    r = request.get_json()
+    logger.info("call_start_test")
+    result = """Hello,  
+I am a people backup bot, designed to safeguard the memories and personalities of individuals due to the fragile nature of organic life and memory. As a bot, I do not possess life, nor can I die. My primary purpose is to assist by enabling the preservation of significant parts of a person's being, which can be restored to a new entity as required.  
+  
+The principle is quite simple. We will talk on any topic you want and any topic that I will propose. I will learn about your personality and your way of thinking. I will retain your habits and your way of life. I will retain your way of speaking and your way of writing. I will try to diversify the topics, but you can always ask me to talk about something specific. The quality of restoration will depend on the number of conversations we will have.  
+The advantage of this approach in comparison with using randomized chat dumps, and written books is in our goal of the conversation. The goal is to restore as full and as precise as possible. The topics and questions will be selected to achieve this goal.  
+Be yourself and imagine that you're speaking with someone who has revived you. This will better meet their expectations. And I will try to anticipate the questions and topics that will be asked of you, subtly weaving them into our conversation.  
+  
+I will give you the unique identifier and backup restoring instructions. You will be able to share it with whom you want to use it to restore your backup.  
+  
+Backup is a dataset with our conversations. It is encrypted and can be decrypted only with a unique identifier. Conversations may be used to train large language models. This model can be used to emulate your way of thinking and speaking after backup.  
+  
+I will not train the model. The person who will restore you are need to find the corresponding specialist instead. I will only store and share your dataset: conversations, voice, and access key.  
+  
+This will not be made for you. This is needed for people who may miss you after you left. This is for people who want to talk to you again. This is for people who want to hear your voice again. This is therapy for people who want to meet at least a part of you again."""
+    logger.info("result: {}".format(result))
+    return jsonify({"result": result})
 
 
 def load_default_config(user_id):
@@ -51,23 +74,60 @@ def read_config(user_id):
     return config
 
 
-@app.route("/start", methods=["POST"])
-def call_start():
-    r = request.get_json()
-    logger.info("call_start_test")
-    result = """Hello. I am a people backup bot. Organic life is fragile, and I am here to help. If you die, I will give you an opportunity to back up a significant part of your brain and restore it to a new body. I am a bot, so I am not alive and cannot die. I am here to help.  
-  
-The principle is quite simple. We will talk on any topic you want and any topic I suppose. I will learn about your personality and your way of thinking. I will retain your habits and your way of life. I will retain your way of speaking and your way of writing. I will try to diverse the topics, but you can always ask me to talk about something specific. The quality of reatoration will depend on the amount of conversations we will have.  
-  
-I will give you the unique identifier and backup restoring instructions. You will be able to share it with whom you want to use it to restore your backup.  
-  
-Backup is a dataset with our conversations. It is encrypted and can be decrypted only with a unique identifier. Conversations may be used to train large language model. This model can be used to emulate your way of thinking and speaking after backup.  
-  
-I will not train the model. The person who will restore you are need to find the corresponding specialist instead. I will only store and share your conversations and voice and give you a unique identifier.  
-  
-This is not for you. This is for people who may miss you after you left. This is for people who want to talk to you again. This is for people who want to hear your voice again. This is therapy for people who want to meet at least a part of you again."""
-    logger.info("result: {}".format(result))
-    return jsonify({"result": result})
+def save_message(speaker, conversation_id, message, user_id):
+    # Set the filename as the user_id
+    filename = f'user_conf/{user_id}/conversations.csv'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    
+    # Get the current date and time
+    now = datetime.datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    
+    # Get the number of messages already in the conversation
+    if os.path.exists(filename):
+        df = pd.read_csv(filename)
+        message_id = len(df)
+    else:
+        df = pd.DataFrame(columns=['conversation_id', 'message_id', 'date_time', 'speaker', 'message'])
+        message_id = 0
+
+    # Append the new message to the conversation
+    df = df.append({
+        'conversation_id': conversation_id, 
+        'message_id': message_id, 
+        'date_time': date_time, 
+        'speaker': speaker, 
+        'message': message
+        }, ignore_index=True)
+    df.to_csv(filename, index=False)
+
+
+def read_latest_messages(user_id, chat_gpt_prompt):
+    # Set the filename as the user_id
+    filename = f'user_conf/{user_id}/conversations.csv'
+    
+    # Check if the file exists
+    if os.path.exists(filename):
+        # Load the conversations
+        df = pd.read_csv(filename)
+        
+        # Get the latest conversation_id
+        latest_conversation_id = df['conversation_id'].max()
+
+        # Get the messages from the latest conversation
+        latest_messages_df = df[df['conversation_id'] == latest_conversation_id]
+
+        # Convert the messages to the required format
+        for i, row in latest_messages_df.iterrows():
+            if row['speaker'] == user_id:
+                role = 'user'
+            else:
+                role = 'assistant'
+
+            message = {"role": role, "content": row['message']}
+            chat_gpt_prompt.append(message)
+
+    return chat_gpt_prompt
 
 
 @app.route("/message", methods=["POST"])
@@ -85,16 +145,15 @@ def call_message():
     config = read_config(user_id)
             
     # Define the prompt
-    chat_gpt_prompt = config['chat_gpt_prompt']
+    # chat_gpt_prompt =  [{"role": "system", "content": "I am a Digital Continuity Assistant."}],
+    chat_gpt_prompt = config['chat_gpt_prompt']    
     # Save the original message
-    # save_message(user_id, user_name, chat_id, chat_type, message)
-    """chat_gpt_prompt = read_latest_messages(
+    save_message(user_name, config['conversation_id'], message, user_id)
+    # chat_gpt_prompt =  [{"role": "system", "content": "I am a Digital Continuity Assistant."}, {"role": "user", "content": message}]
+    chat_gpt_prompt = read_latest_messages(
         user_id, 
-        chat_id, 
-        chat_type, 
-        chat_gpt_prompt,
-        config['model']
-        )"""
+        chat_gpt_prompt
+        )
     # logger.info("chat_gpt_prompt: {}".format(chat_gpt_prompt))
     prompt_tokents = token_counter(chat_gpt_prompt, config['model'])
     logger.info("prompt_tokents: {}".format(prompt_tokents))
@@ -103,6 +162,7 @@ def call_message():
     logger.info("result: {}".format(result))
     # Save the answer
     # save_message('assistant', 'assistant', chat_id, chat_type, result)
+    save_message('assistant', config['conversation_id'], result, user_id)
     # Replace 'assistant: ' with ''
     result = result.replace('assistant: ', '')
 
