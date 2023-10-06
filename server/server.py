7faf71333a50d7d4a8c9ea59fcc470e50315d55f
Alex <format37@gmail.com>
2023-10-06 16:54:19 +0400

    modified:   server/server.py

--- server/server.py
+++ server/server.py
@@ -151,7 +151,7 @@ bots.append(youtubesttbot)
 @youtubesttbot.message_handler(func=lambda message: True, content_types=['text'])
 def youtubesttbot_message(message):
 
-    if not message.text.startswith("https://www.youtube.com/watch"):
+    if not message.text.startswith("https://www.youtube.com/watch") and not message.text.startswith("https://youtu.be/"):
         # youtubesttbot.reply_to(message, "Please send a YouTube video link to transcribe")
         return
 
@@ -185,7 +185,7 @@ def youtubesttbot_message(message):
     # youtubesttbot.send_document(message.chat.id, transcription.encode('utf-8'), caption='Transcription')
     try:
         filename = f'/{uuid.uuid4().hex}.txt'
-        
+
         with open(filename, 'w') as f:
             f.write(transcription)
 
